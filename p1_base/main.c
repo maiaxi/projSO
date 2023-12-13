040fd07b3147887df9e53f84b373320cfe2918d6
maiaxi <jandremaia2003@gmail.com>
2023-12-13 02:43:28 +0000

    tried to implement process

--- p1_base/main.c
+++ p1_base/main.c
@@ -1,11 +1,11 @@
 #include <limits.h>
 #include <stdio.h> // Sofia
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 #include <fcntl.h>
 #include "constants.h"
@@ -44,7 +44,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  
+  int active_children = 0;
   while(1){
     entry = readdir(dir);
     if(entry == NULL){
@@ -61,25 +61,37 @@ int main(int argc, char *argv[]) {
     if(strcmp(dot, ".jobs") != 0){
       continue;
     }
-    input_fd = open(entry->d_name, O_RDONLY);
-    if(input_fd == -1){
+    if(active_children == MAX_PROC){
+      int status;
+      wait(&status);
+      active_children--;
+    }
+    pid_t pid = fork();
+    if(pid == -1){
+      perror("fork");
       continue;
     }
-    printf("%s\n", entry->d_name);
-    char *output_name = malloc(strlen(entry->d_name) + 1);
-    strcpy(output_name, entry->d_name);
-    char *dot2 = strrchr(output_name, '.');
-    *dot2 = '\0';
-    strcat(output_name, ".out");
-    output_fd = open(output_name, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if(pid == 0){
+      printf("Child process with PID %d sarting\n", getpid());
+      input_fd = open(entry->d_name, O_RDONLY);
+      if(input_fd == -1){
+        continue;
+      }
+      printf("%s\n", entry->d_name);
+      char *output_name = malloc(strlen(entry->d_name) + 1);
+      strcpy(output_name, entry->d_name);
+      char *dot2 = strrchr(output_name, '.');
+      *dot2 = '\0';
+      strcat(output_name, ".out");
+      output_fd = open(output_name, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-    if(output_fd == -1){
-      printf("Error opening file %s\n", output_name);
+      if(output_fd == -1){
+        printf("Error opening file %s\n", output_name);
+        free(output_name);
+        close(input_fd);
+        continue;
+      }
       free(output_name);
-      close(input_fd);
-      continue;
-    }
-    free(output_name);
       unsigned int event_id, delay;
       size_t num_rows, num_columns, num_coords;
       size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
@@ -95,7 +107,6 @@ int main(int argc, char *argv[]) {
             if (ems_create(event_id, num_rows, num_columns)) {
               fprintf(stderr, "Failed to create event\n");
             }
-
             break;
           case CMD_RESERVE:
             num_coords = parse_reserve(input_fd, MAX_RESERVATION_SIZE, &event_id, xs, ys);
@@ -108,20 +119,18 @@ int main(int argc, char *argv[]) {
             if (ems_reserve(event_id, num_coords, xs, ys)) {
               fprintf(stderr, "Failed to reserve seats\n");
             }
-
             break;
 
           case CMD_SHOW:
             if (parse_show(input_fd, &event_id) != 0) {
               fprintf(stderr, "Invalid command. See HELP for usage\n");
-              
+                
               continue;
             }
 
             if (ems_show(event_id, output_fd)) {
               fprintf(stderr, "Failed to show event\n");
             }
-
             break;
 
           case CMD_LIST_EVENTS:
@@ -132,16 +141,14 @@ int main(int argc, char *argv[]) {
 
           case CMD_WAIT:
             if (parse_wait(input_fd, &delay, NULL) == -1) {  // thread_id is not implemented
-            
+              
               fprintf(stderr, "Invalid command. See HELP for usage\n");
               continue;
             }
-
             if (delay > 0) {
               printf("Waiting...\n");
               ems_wait(delay);
             }
-
             break;
 
           case CMD_INVALID:
@@ -158,7 +165,6 @@ int main(int argc, char *argv[]) {
                 "  WAIT <delay_ms> [thread_id]\n"  // thread_id is not implemented
                 "  BARRIER\n"                      // Not implemented
                 "  HELP\n");
-
             break;
 
           case CMD_BARRIER:  // Not implemented
@@ -170,14 +176,25 @@ int main(int argc, char *argv[]) {
             break;
         }
         if(ended){
-          close(input_fd);
-          close(output_fd);
           break;
         }
       }
+      close(input_fd);
+      close(output_fd);
+      printf("Child process with PID %d finished\n", getpid());
+    }
+    else{
+      active_children++;
+    }
   }
+  //wait for all the children to finish
+
+  while (active_children > 0){
+    int status;
+    wait(&status);
+    active_children--;
+  }  
   closedir(dir);
   ems_terminate();
   return 0;
-
 }
