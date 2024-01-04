a4aa7e6d8ffbbe7fa12c37350465ff24a28616fd
maiaxi <jandremaia2003@gmail.com>
2024-01-04 23:57:02 +0000

    Started working on pipes. Logic almost done

--- p2_base/client/api.c
+++ p2_base/client/api.c
@@ -1,12 +1,102 @@
 #include "api.h"
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define FIFO_PATHNAME "fifo.pipe"
+
 
 int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const* server_pipe_path) {
   //TODO: create pipes and connect to the server
-  return 1;
+  // Remove pipe if it does not exist
+  if (unlink(req_pipe_path) != 0 && errno != ENOENT) {
+      fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", req_pipe_path,
+              strerror(errno));
+      exit(EXIT_FAILURE);
+  }
+  // Create pipe
+  if (mkfifo(req_pipe_path, 0640) != 0) {
+      fprintf(stderr, "[ERR]: mkfifo failed: %s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+  }
+
+
+
+  // Remove pipe if it does not exist
+  if (unlink(resp_pipe_path) != 0 && errno != ENOENT) {
+      fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", resp_pipe_path,
+              strerror(errno));
+      exit(EXIT_FAILURE);
+  }
+  // Create pipe
+  if (mkfifo(resp_pipe_path, 0640) != 0) {
+      fprintf(stderr, "[ERR]: mkfifo failed: %s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+  }
+
+  //opens the server pipe to send the message code to the server
+  /*(char) OP_CODE=1 | (char[40]) nome do pipe do cliente (para pedidos) | (char[40]) nome do pipe do cliente (para
+respostas)*/
+
+  int server_fd = open(server_pipe_path, O_WRONLY);
+  if (server_fd == -1) {
+      fprintf(stderr, "[ERR]: open(%s) failed: %s\n", server_pipe_path,
+              strerror(errno));
+      exit(EXIT_FAILURE);
+  }
+  //creates a buffer to send the message code to the server
+  char buffer[81];
+  memset(buffer, 0, 81);
+
+
+  //writes the message code to the buffer
+  buffer[0] = '1';
+
+  //writes the request pipe path to the buffer
+  for(int i = 0; i < 40; i++){
+    buffer[i+1] = req_pipe_path[i];
+  }
+  //writes the response pipe path to the buffer
+  for(int i = 0; i < 40; i++){
+    buffer[i+41] = resp_pipe_path[i];
+  }
+
+
+  //writes the buffer to the server pipe
+  if (write(server_fd, buffer, 81) != 81) {
+      fprintf(stderr, "[ERR]: write(%s) failed: %s\n", server_pipe_path,
+              strerror(errno));
+      exit(EXIT_FAILURE);
+  }
+  //listens to the response pipe waiting for a integer response from the server
+  int resp_fd = open(resp_pipe_path, O_RDONLY);
+  if (resp_fd == -1) {
+      fprintf(stderr, "[ERR]: open(%s) failed: %s\n", resp_pipe_path,
+              strerror(errno));
+      exit(EXIT_FAILURE);
+  }
+  //reads the response from the server
+  int resp;
+  if (read(resp_fd, &resp, sizeof(int)) == -1) {
+      fprintf(stderr, "[ERR]: read(%s) failed: %s\n", resp_pipe_path,
+              strerror(errno));
+      exit(EXIT_FAILURE);
+  }
+  printf("resp: %d\n", resp);
+  return 0;
 }
 
 int ems_quit(void) { 
   //TODO: close pipes
+  //send 
   return 1;
 }
 
@@ -27,5 +117,7 @@ int ems_show(int out_fd, unsigned int event_id) {
 
 int ems_list_events(int out_fd) {
   //TODO: send list request to the server (through the request pipe) and wait for the response (through the response pipe)
+  
+  
   return 1;
 }
--- p2_base/server/main.c
+++ p2_base/server/main.c
@@ -1,10 +1,34 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <string.h>
+
+
 
 #include "common/constants.h"
 #include "common/io.h"
 #include "operations.h"
+#include "server/eventlist.h"
+
+
+#define NUM_WORKER_THREADS 4
+
+typedef struct {
+  int sess_id;
+  char* req_pipe_path[41];
+  char* resp_pipe_path[41];
+} client_t;
+
+int num_clients = 0;
 
 int main(int argc, char* argv[]) {
   if (argc < 2 || argc > 3) {
@@ -29,12 +53,95 @@ int main(int argc, char* argv[]) {
     fprintf(stderr, "Failed to initialize EMS\n");
     return 1;
   }
-
+  //start an array of clients to keep track of them
+  client_t* clients;
   //TODO: Intialize server, create worker threads
+  if(unlink(argv[1]) != 0 && errno != ENOENT){
+    fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", argv[1], strerror(errno));
+    exit(EXIT_FAILURE);
+  }
 
+  if(mkfifo(argv[1], 0640) != 0){
+    fprintf(stderr, "[ERR]: mkfifo failed: %s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+  
   while (1) {
     //TODO: Read from pipe
     //TODO: Write new client to the producer-consumer buffer
+    //waits for a client to connect
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+      fprintf(stderr, "[ERR]: open(%s) failed: %s\n", argv[1], strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+    char* msg = malloc(81);
+    memset(msg, 0, 81);
+    printf("Waiting for client...\n");
+    if (read(fd, msg, 81) == -1) {
+      fprintf(stderr, "[ERR]: read(%s) failed: %s\n", argv[1], strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+    printf("Client connected!\n");
+    switch(msg[0]){
+      case '1':
+        //parse message
+        char* req_pipe_path = malloc(41);
+        char* resp_pipe_path = malloc(41);
+        for(int i = 0; i < 40; i++){
+          req_pipe_path[i] = msg[i+1];
+          resp_pipe_path[i] = msg[i+41];
+        }
+        if(num_clients == 0){
+          clients = malloc(sizeof(client_t));
+        }
+        else{
+          clients = realloc(clients, sizeof(client_t)*(num_clients+1));
+        }
+        num_clients++;
+        int client_id = num_clients;
+
+        clients[client_id-1].sess_id = client_id;
+        strcpy(clients[client_id-1].req_pipe_path, req_pipe_path);
+        strcpy(clients[client_id-1].resp_pipe_path, resp_pipe_path);
+
+        //send response with the session id (int)
+        //opens the client pipe to send the session id
+        int client_fd = open(resp_pipe_path, O_WRONLY);
+        if (client_fd == -1) {
+          fprintf(stderr, "[ERR]: open(%s) failed: %s\n", req_pipe_path, strerror(errno));
+          exit(EXIT_FAILURE);
+        }
+        //writes the session id to the client
+        if (write(client_fd, &client_id, sizeof(int)) == -1) {
+          fprintf(stderr, "[ERR]: write(%s) failed: %s\n", req_pipe_path, strerror(errno));
+          exit(EXIT_FAILURE);
+        }
+
+
+        break;
+      case '2':
+        printf("Client wants to unregister!\n");
+        break;
+      case '3':
+        printf("Client wants to list!\n");
+        break;
+      case '4':
+        printf("Client wants to get!\n");
+        break;
+      case '5':
+        printf("Client wants to put!\n");
+        break;
+      case '6':
+        printf("Client wants to sync!\n");
+        break;
+      case '7':
+        printf("Client wants to shutdown!\n");
+        break;
+      default:
+        printf("Invalid message code!\n");
+        break;
+    }
   }
 
   //TODO: Close Server
